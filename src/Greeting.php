c6d0a32ff3ec58c98cee4ff87e469adde1b67d1b
Panibrat <a.panibratenko@gmail.com>
2016-05-26 11:21:21 +0300

    final release

--- src/Greeting.php
+++ src/Greeting.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Description of Greeting
- * http://www.quotationspage.com/
- * @author cpaniba
- */
-require_once 'RandomQuote.php';
+
 class Greeting {
         use RandomQuote;
         public function say($name)
